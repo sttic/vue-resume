9efcb41825a2390c91c050f5b023fc381bc8cdd0
sttic <tommyd3689@gmail.com>
2019-01-15 21:39:54 -0500

    structure and content

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -5,91 +5,133 @@
     </div>
     <div class="row">
       <div class="column left">
+        <!-- EDUCATION -->
         <section>
           <h2>Education</h2>
-          <h3>University of Ottawa</h3>
-          <h4>BASc Software Engineering</h4>
-          <h5>2nd Year - Expected Dec 2021
-            <br>CGPA: 9.2/10 (Dean’s Honour List)
+          <h3>{{ education.school }}</h3>
+          <h4>{{ education.degree }}</h4>
+          <h5>
+            {{ education.progress }} - Expected {{ education.expected }}
+            <br>
+            CGPA: {{ education.cgpa }}
           </h5>
         </section>
+
+        <!-- CONTACT -->
         <section>
           <h2>Contact</h2>
           <ul>
-            <li>
-              <a href>tdeng075@uottawa.ca</a>
-            </li>
-            <li>
-              <p>613-606-7742</p>
+            <li v-for="item in contacts" :key="item">
+              <a :href="item.href">{{ item.display }}</a>
             </li>
           </ul>
         </section>
+
+        <!-- LINKS -->
         <section>
           <h2>Links</h2>
           <ul>
-            <li>
-              <a href>tommydeng.com</a>
-            </li>
-            <li>
-              <a href>github.com/sttic</a>
-            </li>
-            <li>
-              <a href>linkedin.com/in/tommydeng</a>
+            <li v-for="item in links" :key="item">
+              <a :href="item.href">{{ item.display }}</a>
             </li>
           </ul>
         </section>
+
+        <!-- SKILLS -->
         <section>
           <h2>Skills</h2>
-          <h5>Languages</h5>
-          <h5>Libraries</h5>
-          <h5>Technologies</h5>
-          <h5>Development</h5>
+          <div v-for="(skill, index) in skills" :key="skill">
+            <h5 style="text-transform: capitalize;">{{ index }}</h5>
+            <p v-for="item in skill" :key="item">
+              <span v-for="(language, index) in item" :key="index">
+                <span v-if="index != 0">&bull;</span>
+                {{ language }}
+              </span>
+            </p>
+          </div>
         </section>
+
+        <!-- HACKATHONS -->
         <section>
           <h2>Hackathons</h2>
           <ul>
-            <li>2018 <span>Hack Western</span></li>
+            <li v-for="item in hackathons" :key="item">
+              {{ item.date }}
+              <span>{{ item.name }}</span>
+            </li>
           </ul>
         </section>
+
+        <!-- AWARDS -->
         <section>
           <h2>Awards</h2>
           <ul>
-            <li>2018 <span>Merit Scholarship</span></li>
+            <li v-for="item in awards" :key="item">
+              {{ item.date }}
+              <span>{{ item.name }}</span>
+            </li>
           </ul>
         </section>
+
+        <!-- INTERESTS -->
         <section>
           <h2>Interests</h2>
+          <p v-for="item in interests" :key="item">
+            <span v-for="(interest, index) in item" :key="index">
+              <span v-if="index != 0">&bull;</span>
+              {{ interest }}
+            </span>
+          </p>
         </section>
       </div>
+
       <div class="column right">
+        <!-- WORK EXPERIENCE -->
         <section>
           <h2>Work Experience</h2>
-        </section>
-        <section>
-          <h2>Technical and Leadership Experience</h2>
-          <div>
-            <h3>Ottabotics Robot Racing |
-              <h4 style="display: inline;">Robotics Competition Team</h4>
+          <div v-for="item in work_experience" :key="item">
+            <h3>
+              {{ item.name }} |
+              <h4 style="display: inline;">{{ item.position }}</h4>
             </h3>
-            <h5>Sep 2017 – Present | Ottawa, ON</h5>
+            <h5>{{ item.start }} – {{ item.end }} | {{ item.location }}</h5>
             <ul>
-              <li>Automated Blender with Python to render photorealistic videos of racing tracks for use as training data and driving simulation for autonomous vehicle</li>
-              <li>Created computer vision system to detect traffic light states</li>
-              <li>Wrote script that undistorts wide-angle camera images</li>
-              <li>Developed compression system to send differences between the current and previous frame in a video feed to reduce bandwidth usage in video streaming</li>
+              <li v-for="point in item.details" :key="point">{{ point }}</li>
             </ul>
           </div>
         </section>
+
+        <!-- TECHNICAL AND LEADERSHIP EXPERIENCE -->
+        <section>
+          <h2>Technical and Leadership Experience</h2>
+          <div v-for="item in other_experience" :key="item">
+            <h3>
+              {{ item.name }} |
+              <h4 style="display: inline;">{{ item.position }}</h4>
+            </h3>
+            <h5>{{ item.start }} – {{ item.end }} | {{ item.location }}</h5>
+            <ul>
+              <li v-for="point in item.details" :key="point">{{ point }}</li>
+            </ul>
+          </div>
+        </section>
+
+        <!-- PROJECTS -->
         <section>
           <h2>Projects</h2>
-          <div>
-            <h3>Insta Eats
-              <h5 style="display: inline;">Hack Western, London | Nov 2018</h5>
+          <div v-for="item in projects" :key="item">
+            <h3>
+              {{ item.name }}
+              <h5 style="display: inline;">{{ item.tag }} | {{ item.date }}</h5>
             </h3>
+            <ul>
+              <li v-for="point in item.details" :key="point">{{ point }}</li>
+            </ul>
           </div>
         </section>
       </div>
     </div>
+
     <div class="footer">
       Please see
       <a href>tommydeng.com</a> for projects
@@ -100,7 +142,184 @@
 <script>
 export default {
   name: "Resume",
-  props: {}
+  props: {},
+  data() {
+    return {
+      education: {
+        school: "University of Ottawa",
+        degree: "BASc Software Engineering",
+        progress: "2nd Year",
+        expected: "Dec 2021",
+        cgpa: "9.2/10 (Dean's Honour List)"
+      },
+      contacts: [
+        { href: "mailto:tdeng075@uottawa.ca", display: "tdeng075@uottawa.ca" }
+      ],
+      links: [
+        { href: "http://tommydeng.com/", display: "tommydeng.com" },
+        { href: "https://github.com/sttic", display: "github.com/sttic" },
+        {
+          href: "https://www.linkedin.com/in/tommydeng/",
+          display: "linkedin.com/in/tommydeng"
+        }
+      ],
+      skills: {
+        languages: [
+          ["Python", "Java", "C++", "C", "C#"],
+          ["JavaScript", "TypeScript"]
+        ],
+        libraries: [
+          ["OpenCV", "Numpy", "Selenium"],
+          ["Keras", "PyAutoGUI", "Pillow"]
+        ],
+        technologies: [
+          ["Vue", "React", "NodeJS", "Django"],
+          ["LaTeX", "Markdown", "HTML", "CSS"],
+          ["Virtual Machines", "Linux", "Git"],
+          ["VS Code", "Visual Studio", "Eclipse"],
+          ["Android Studio", "Unity 3D"],
+          ["Blender", "Adobe CC"]
+        ],
+        development: [
+          ["Agile", "CI", "VC", "UML"],
+          ["Data Structures & Algorithms"],
+          ["Object Oriented Design"]
+        ]
+      },
+      hackathons: [
+        { name: "ConUHacks", date: "2019" },
+        { name: "Hack Western", date: "2018" },
+        { name: "Hack the North", date: "2018" },
+        { name: "CU Hacks", date: "2018" },
+        { name: "UOttaHack", date: "2018" }
+      ],
+      awards: [
+        { name: "Merit Scholarship", date: "2018" },
+        { name: "$4000 Admission Scholarship", date: "2017" }
+      ],
+      interests: [
+        ["Computer Vision", "Data Science"],
+        ["Robotics", "Machine Learning"],
+        ["Web Development"]
+      ],
+      work_experience: [
+        {
+          name: "Ross Video",
+          position: "Software Developer",
+          start: "Jan 2019",
+          end: "Apr 2019",
+          location: "Ottawa, ON",
+          details: []
+        },
+        {
+          name: "Global Affairs Canada",
+          position: "Software Quality Control",
+          start: "May 2018",
+          end: "Aug 2018",
+          location: "Ottawa, ON",
+          details: [
+            "Read and followed system design diagrams for the EICS II project",
+            "Became proficient in Microsoft Test Manager and Team Foundation Server for executing quality control tests",
+            "Trained new consultants on toolset usage, testing workflow, and system requirements"
+          ]
+        }
+      ],
+      other_experience: [
+        {
+          name: "Ottabotics Robot Racing",
+          position: "Robotics Competition Team",
+          start: "Sep 2017",
+          end: "Present",
+          location: "Ottawa, ON",
+          details: [
+            "Automated Blender with Python to render photorealistic videos of racing tracks for use as training data and driving simulation for autonomous vehicle",
+            "Created computer vision system to detect traffic light states",
+            "Wrote script that undistorts wide-angle camera images",
+            "Developed compression system to send differences between the current and previous frame in a video feed to reduce bandwidth usage in video streaming"
+          ]
+        },
+        {
+          name: "Inventure Accelerator",
+          position: "Media and Outreach",
+          start: "Jan 2018",
+          end: "Present",
+          location: "Ottawa, ON",
+          details: [
+            "Managed and updated the company website, meetinventure.com",
+            "Improved user experience by continuously communicating with clients",
+            "Organized the four-hour SparkFest 2018 event with the Inventure team",
+            "Created SparkFest sponsor video featuring Google and Invest Ottawa"
+          ]
+        }
+      ],
+      projects: [
+        {
+          name: "Insta Eats",
+          tag: "Hack Western, London",
+          date: "Nov 2018",
+          details: [
+            "Developed web service that provides tailored restaurant recommendations based on the user’s Instagram photos using React and Google Vision API"
+          ]
+        },
+        {
+          name: "Get A Goose",
+          tag: "Hack the North, Waterloo",
+          date: "Sept 2018",
+          details: [
+            "Constructed speech-to-text captioning system that broadcasts parsed speech from Android app to website using Google Voice API and Firebase database"
+          ]
+        },
+        {
+          name: "Image Classifier",
+          tag: "Personal Project",
+          date: "Sept 2018",
+          details: [
+            "Applied Keras TensorFlow API to design and train a neural network to classify images of handwritten digits with >98% accuracy"
+          ]
+        },
+        {
+          name: "Object Detection",
+          tag: "Personal Project",
+          date: "July 2018",
+          details: [
+            "Adapted YOLOv3 object detection system to localize common road objects"
+          ]
+        },
+        {
+          name: "HQ Trivia Bot",
+          tag: "Personal Project",
+          date: "May 2018",
+          details: [
+            'Automated Google search and parsing for "HQ" Trivia using Selenium'
+          ]
+        },
+        {
+          name: "Tic Tac Voice",
+          tag: "UOttaHack, Ottawa",
+          date: "Feb 2018",
+          details: [
+            "Built an accessibility mobile app aimed towards the visually impaired using Android Studio and Google Voice API"
+          ]
+        },
+        {
+          name: "Life's Charge Visualization",
+          tag: "Personal Project",
+          date: "Aug 2017",
+          details: [
+            "Generated animated graphic representing a typical lifespan using CImg in C++"
+          ]
+        },
+        {
+          name: "Potato Simulator",
+          tag: "ICS3U",
+          date: "Jun 2016",
+          details: [
+            "Designed Unity 3D game in C# about a personified potato roaming around an open world"
+          ]
+        }
+      ]
+    };
+  }
 };
 </script>
 
