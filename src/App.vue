5bc3f09af8d7a4997edc69ef18fe1e262a6b9d77
sttic <tommyd3689@gmail.com>
2019-01-19 22:42:28 -0500

    functional scaling

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <Resume :style="`transform: translateX(-50%) scale(${scale});`"/>
+  <div id="app" :style="`height: ${appHeight}px;`">
+    <Resume :style="`transform: translate(-50%, -50%) scale(${scale});`"/>
   </div>
 </template>
 
@@ -20,23 +20,47 @@ export default {
   },
   methods: {
     handleResize() {
-      // eslint-disable-next-line
-      console.log(`${window.innerWidth} ${window.innerHeight}`);
-
-      this.scale = Math.min(window.innerWidth / 1029, 1);
-      // eslint-disable-next-line
-      console.log(this.scale);
+      this.windowWidth = window.innerWidth;
+      this.windowHeight = window.innerHeight;
     }
   },
   data() {
     return {
-      scale: 1
+      letterInch: { width: 8.5, height: 11 },
+      inch: 96,
+      padX: 0.5,
+      padY: 0.5,
+      windowWidth: 0,
+      windowHeight: 0
     };
+  },
+  computed: {
+    scale() {
+      var compensation = 0.5;
+      return Math.min(
+        this.windowWidth / ((8.5 + 2 * this.padX + compensation) * this.inch),
+        1
+      );
+    },
+    resumeWidth() {
+      return this.letterInch.width * this.inch * this.scale;
+    },
+    resumeHeight() {
+      return this.letterInch.height * this.inch * this.scale;
+    },
+    appHeight() {
+      return Math.max(
+        this.resumeHeight + 2 * this.padY * this.inch,
+        this.windowHeight
+      );
+    }
   }
 };
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Lato:200,300,400,700|Raleway:600");
+
 body {
   margin: 0;
 }
@@ -45,16 +69,15 @@ body {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: black;
   position: relative;
-  padding: 0.5in 0;
-  /* height: 100%; */
 }
 
 #resume {
-  position: relative;
+  width: 8.5in;
+  height: 11in;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.08), 0 6px 32px 0 rgba(0, 0, 0, 0.08);
+  position: absolute;
   left: 50%;
-  /* top: 50%; */
-  /* transform-origin: center center; */
+  top: 50%;
 }
 </style>
