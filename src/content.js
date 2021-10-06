3079f12034c7c4d9a8ac1e5a2249418b69e3e13f
Tommy Deng <git@tommydeng.com>
2021-10-06 00:43:57 -0400

    update content:
    - add Amazon
    - remove Global Affairs
    - update dates
    - misc tweaks

--- src/content.js
+++ src/content.js
@@ -33,27 +33,32 @@ export default {
     title: "Skills",
     content: [
       {
+        category: "Teamwork",
+        groups: [
+          [
+            "Communication",
+            "&nbsp;&nbsp;&nbsp;&nbsp;Agile",
+            "Problem-solving",
+            "&nbsp;&nbsp;&nbsp;&nbsp;Ownership",
+            "User-centered",
+            "&nbsp;&nbsp;&nbsp;&nbsp;Inventive"
+          ]
+        ]
+      },
+      {
         category: "Languages",
-        groups: [["Python", "Java", "JavaScript", "TypeScript", "HTML", "CSS"]]
+        groups: [
+          ["Python", "Java", "C++", "JavaScript", "TypeScript", "HTML/CSS"]
+        ]
       },
       {
         category: "Technologies",
         groups: [
-          [
-            "React",
-            "Vue",
-            "Next.js",
-            "NodeJS",
-            "PostgreSQL",
-            "Express",
-            "Jest",
-            "Selenium"
-          ],
+          ["React", "Vue", "Next.js", "NodeJS", "PostgreSQL", "Express"],
           ["GCP", "Firebase", "AWS", "Docker"],
-          ["Git", "Jira", "Linux", "Bash", "Visual Studio", "Eclipse"]
+          ["Git", "Jira", "Linux", "Bash", "Visual Studio", "Figma"]
         ]
-      },
-      { category: "Development", groups: [["Agile", "CI", "TDD", "UML"]] }
+      }
     ]
   },
   hackathons: {
@@ -83,6 +88,16 @@ export default {
     title: "Work Experience",
     content: [
       {
+        title_primary: "Amazon Web Services (AWS)",
+        website: "https://aws.amazon.com/",
+        title_secondary: "SDE Intern",
+        date: "May - Aug 2021",
+        details: [
+          "Enhanced discoverability of cloud resources by implementing metadata additions across <b>Lambda</b> API, <b>CloudFormation</b>, and <b>React UI</b> in <b>Java</b> and <b>TypeScript</b>",
+          "Enabled advanced querying and 10x faster search times by replicating database to <b>Elasticsearch</b> using <b>DynamoDB Streams</b>, <b>SQS</b>, and <b>Lambda</b>"
+        ]
+      },
+      {
         title_primary: "Bank of Montreal",
         website: "https://www.bmo.com/",
         title_secondary: "Capital Markets Developer",
@@ -99,8 +114,8 @@ export default {
         title_secondary: "Analytics Software Developer",
         date: "Sep - Dec 2019",
         details: [
-          "Dramatically accelerated product timeline to meet target release date by producing above-average >15 user-facing visualization and UI/UX features in <b>React-TypeScript</b>, while following test-driven development using <b>Selenium</b> and <b>Jest</b>",
-          "Enhanced product design by integrating feedback from client and domain expert demos"
+          "Dramatically accelerated product timeline to meet release by developing above-average >15 user-facing visualization and UI/UX features in <b>React</b>, <b>TypeScript</b>, <b>D3.js</b>, and <b>Cytoscape</b> while following test-driven development using <b>Selenium</b> and <b>Jest</b>",
+          "Enhanced product design by integrating feedback from client and domain experts"
         ]
       },
       {
@@ -112,16 +127,6 @@ export default {
           "Increased <b>Jenkins</b> test suite execution speeds by >200% and enabled reliable headless operation by overhauling the continuous integration process with Robot Framework",
           "Allowed testing of previously blocked video control and monitoring features by adding key functionalities to internally developed automation libraries using <b>Java</b>"
         ]
-      },
-      {
-        title_primary: "Global Affairs Canada",
-        website: "https://www.international.gc.ca/gac-amc/index.aspx?lang=eng",
-        title_secondary: "Software QA Analyst",
-        date: "May - Aug 2018",
-        details: [
-          "Surpassed QA targets and achieved 100% coverage for each testing cycle",
-          "Trained consultants on toolset usage, testing workflow, and system requirements"
-        ]
       }
     ]
   },
@@ -130,19 +135,18 @@ export default {
     content: [
       {
         title_primary: "CUSEC",
-        website: "https://www.linkedin.com/company/cusec/",
+        website: "https://2021.cusec.net/",
         title_secondary: "Director of Technology & Design",
-        date: "Apr 2020 - Present",
+        date: "Apr 2020 - Jan 2021",
         details: [
-          "Attracted sponsors and attendees by designing a modern brand identity to reach a broader audience and follow web standards, using <b>Figma</b> and <b>React</b>",
-          "Conducted cross-team design and product demos to gather and satisfy user needs"
+          "Attracted sponsors and attendees by designing a modern brand identity to reach an international audience of >1000 users, using <b>Figma</b>, <b>Next.js</b>, and <b>Chakra UI</b>"
         ]
       },
       {
         title_primary: "uOttaHack",
-        website: "https://www.linkedin.com/company/uottahack/",
+        website: "https://2021.uottahack.ca/",
         title_secondary: "Developer Team Lead",
-        date: "Nov 2019 - Present",
+        date: "Nov 2019 - Feb 2021",
         details: [
           "Served and collected hackathon registration data from thousands of users by effectively collaborating with developers in creating multiple attendee-facing apps using <b>React</b>, <b>Next.js</b>, and <b>Firebase</b> (landing page, application portal, and live schedule)"
         ]
